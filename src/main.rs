5a4a1381c9d5570e1ce4c615c5de1b1823dbeed1
Mark Goodwin <m0good04@gmail.com>
2025-12-10 10:11:58 -0500

    Implemented a crude text editor component.

--- src/main.rs
+++ src/main.rs
@@ -27,25 +27,65 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
-
+        TextEditor {}
     }
 }
 
 #[component]
-pub fn Hero() -> Element {
+pub fn TextEditor() -> Element {
+    let mut content = use_signal(|| String::new());
+    let mut filename = use_signal(|| String::from("untitled.txt"));
+
     rsx! {
         div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            class: "text-editor",
+            div {
+                class: "toolbar",
+                input {
+                    r#type: "text",
+                    value: "{filename}",
+                    oninput: move |e| filename.set(e.value()),
+                    placeholder: "Filename"
+                }
+                button {
+                    onclick: move |_| {
+                        if let Err(e) = save_file(&filename(), &content()) {
+                            eprintln!("Error saving file: {}", e);
+                        }
+                    },
+                    "Save"
+                }
+                button {
+                    onclick: move |_| {
+                        match load_file(&filename()) {
+                            Ok(text) => content.set(text),
+                            Err(e) => eprintln!("Error loading file: {}", e),
+                        }
+                    },
+                    "Load"
+                }
+            }
+            textarea {
+                class: "editor-area",
+                value: "{content}",
+                oninput: move |e| content.set(e.value()),
+                rows: "20",
+                cols: "80"
             }
         }
     }
+}
+
+fn save_file(filename: &str, content: &str) -> std::io::Result<()> {
+    use std::fs;
+    let config = global::Global::get().expect("Config not initialized");
+    let file_path = config.documents_dir.join(filename);
+    fs::write(file_path, content)
+}
+
+fn load_file(filename: &str) -> std::io::Result<String> {
+    use std::fs;
+    let config = global::Global::get().expect("Config not initialized");
+    let file_path = config.documents_dir.join(filename);
+    fs::read_to_string(file_path)
 }
